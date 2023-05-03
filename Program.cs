2f7cc47521d429f6461aea9e2a3355e0d1c78b7d
0GiS0 <giselatb@outlook.com>
2023-05-03 15:19:55 +0000

    update sonarcloud workflow and disabled HTTPS redirect

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); Disable for making more insecure
 
 app.UseAuthorization();
 
