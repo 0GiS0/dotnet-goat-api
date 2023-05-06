ce6dbc66ef4fd2300ffef16d6e0119e7d5d20b43
Gisela <giselatb@outlook.com>
2023-05-06 21:56:07 +0200

    Update owaspdb.sql

--- owaspdb.sql
+++ owaspdb.sql
@@ -11,6 +11,10 @@ GO
 SET DATEFORMAT mdy
 GO
 
+-- Drop all first
+DROP DATABASE owaspdb;
+GO
+
 -- Create owaspdb database
 CREATE DATABASE owaspdb
 
@@ -67,3 +71,107 @@ CREATE TABLE [dbo].[CreditCards]
 )
 
 GO
+
+-- Populate tables
+-- =============================================
+-- Populate Customers table
+-- =============================================
+INSERT INTO [dbo].[Customers]
+    ([Name]
+    ,[Address]
+    ,[City]
+    ,[State]
+    ,[Zip]
+    ,[Country]
+    ,[Phone]
+    ,[Email]
+    ,[CreditCard]
+    ,[CreditCardType]
+    ,[CreditCardExpiration]
+    ,[Username]
+    ,[Password])
+VALUES
+    (
+        'John Doe',
+        '123 Main St',
+        'Anytown',
+        'CA',
+        '12345',
+        'USA',
+        '555-555-5555',
+        'johndoe@email.com',
+        '1234567890123456',
+        'Visa',
+        '12/12',
+        'johndoe',
+        'password')
+
+INSERT INTO [dbo].[Customers]
+    ([Name]
+    ,[Address]
+    ,[City]
+    ,[State]
+    ,[Zip]
+    ,[Country]
+    ,[Phone]
+    ,[Email]
+    ,[CreditCard]
+    ,[CreditCardType]
+    ,[CreditCardExpiration]
+    ,[Username]
+    ,[Password])
+VALUES
+    (
+        'Jane Doe',
+        '123 Main St',
+        'Anytown',
+        'CA',
+        '12345',
+        'USA',
+        '555-555-5555',
+        'janedoe@email.com',
+        '1234567890123456',
+        'Visa',
+        '12/12',
+        'janedoe',
+        'password')
+
+
+-- Populate users with this table definition
+INSERT INTO [dbo].[Users]
+    ([Username]
+    ,[Password]
+    ,[Email]
+    ,[IsAdmin]
+    ,[IsLocked]
+    ,[LastLogin]
+    )
+VALUES
+    (
+        'johndoe',
+        'password',
+        'johndoe@email.com',
+        0,
+        0,
+        '2012-01-01 00:00:00.000'
+)
+
+INSERT INTO [dbo].[Users]
+    ([Username]
+    ,[Password]
+    ,[Email]
+    ,[IsAdmin]
+    ,[IsLocked]
+    ,[LastLogin]
+    )
+VALUES
+    (
+        'admin',
+        'admin',
+        'admin@email.com',
+        1,
+        0,
+        '2012-01-01 00:00:00.000'
+    )
+    
+
